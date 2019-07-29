9ba85b1f87846d050a9b0d527701cf04c8ddac2a
Darian Tavana <datavana@gmail.com>
2019-07-29 18:18:50 -0400

    check if the request is sent byu the superUser

---
+++ middleware/checkSuperUser.js
@@ -0,0 +1,14 @@
+const checkSuperUser = (req, res, next) => {
+    const authToken = req.token;
+    if(authToken !== process.env.SUPER_TOKEN) {
+        res.status(403).send({
+            success: false,
+            msg: "Invalid token"
+        });
+    } else {
+        next();
+    };
+    
+};
+
+module.exports = checkSuperUser;
