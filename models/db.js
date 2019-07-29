f42124795637e837ca907e9206ffaac983e59e7e
Darian Tavana <datavana@gmail.com>
2019-07-29 18:18:57 -0400

    change to postgres

--- models/db.js
+++ models/db.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECTION_STRING, {useNewUrlParser: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-module.exports = db;
+
+const Pool = require('pg').Pool;
+function init() {
+    const pool = new Pool({});
+    return pool;
+}
+
+module.exports = init();
