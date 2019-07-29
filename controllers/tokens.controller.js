f02558b284f73b08ad3907d2c7c416573b703745
Darian Tavana <datavana@gmail.com>
2019-07-29 18:18:39 -0400

    add generateToken method

---
+++ controllers/tokens.controller.js
@@ -0,0 +1,19 @@
+module.exports = {
+    generateToken: (req, res, next) => {
+        const { userId } = req.body;
+        if(!userId) {
+            res.status(401).send({
+                success: false,
+                msg: "Invalid auth token"
+            });
+        } else {
+            const token = jwt.sign({ userId: userId }, process.env.JWT_SECRET);
+            res.status(200).send({
+                success: true,
+                msg: `Token generated for ${userId}`,
+                userId: userId,
+                token: token
+            });
+        };
+    },
+}
