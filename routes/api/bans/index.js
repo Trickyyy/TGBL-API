4e7776d36f1b1dc93abe70caa3ff11844e0a482e
Darian Tavana <datavana@gmail.com>
2019-07-29 18:45:39 -0400

    add addBan route

--- routes/api/bans/index.js
+++ routes/api/bans/index.js
@@ -1,6 +1,8 @@
 const router = express.Router();
 const checkToken = require('../../../middleware/checkToken');
+const checkSuperUser = require('../../../middleware/checkSuperUser');
 router.use('/getBanned', checkToken, require('./getBanned'));
 router.use('/getBans', checkToken, require('./getBans'));
+router.use('/addBan', checkSuperUser, require('./addBan'));
 
 module.exports = router;
