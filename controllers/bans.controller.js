18daf4fa23f72a6884a8eab0a477bf7039dbb2fa
Darian Tavana <datavana@gmail.com>
2019-07-29 18:18:21 -0400

    add bans methods

--- controllers/bans.controller.js
+++ controllers/bans.controller.js
@@ -1,10 +1,44 @@
+const dbController = require('./db.controller');
+
 module.exports = {
     getBanned: (req, res, next) => {
         const db = req.app.locals.db;
+        /*
         const authToken = req.token;
         const decodedToken = req.decoded;
-
-        
-        
-    }
+        */
+        const user = req.body.user;
+        if(!user) {
+            res.status(401).send({
+                success: false,
+                msg: "No user specified"
+            });
+        } else {
+            const isBanned = dbController.selectSingle(db, "SELECT * FROM bans WHERE user = $1 AND isBanned = true;", user);
+            if(isBanned) {
+                res.status(200).send({
+                    success: true,
+                    isBanned = true,
+                    data: isBanned
+                });
+            } else {
+                res.status(200).send({
+                    success: true,
+                    isBanned = false
+                });
+            };
+        };
+    },
+    getBans: (req, res, next) => {
+        const db = req.app.locals.db;
+        /*
+        const authToken = req.token;
+        const decodedToken = req.decoded;
+        */
+        const bansList = dbController.selectMultiple(db, "SELECT * FROM bans;");
+        res.status(200).send({
+            success: true,
+            data: bansList
+        });
+    },
 }
