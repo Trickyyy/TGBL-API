db33da812cd1636309ec7b746dcd28842180c62f
Darian Tavana <datavana@gmail.com>
2019-08-16 23:16:11 -0400

    remove debug log

--- middleware/checkSuperUser.js
+++ middleware/checkSuperUser.js
@@ -1,7 +1,5 @@
 const checkSuperUser = (req, res, next) => {
     const authToken = req.token;
-    console.log(authToken);
-    console.log(process.env.SUPER_TOKEN);
     if(authToken !== process.env.SUPER_TOKEN) {
         res.status(403).send({
             success: false,
