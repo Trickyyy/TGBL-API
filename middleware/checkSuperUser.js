3c62a119349adabcf46368fb94b0a88af35e16d9
Darian Tavana <datavana@gmail.com>
2019-08-16 22:34:14 -0400

    change error msg for checkSuperUser

--- middleware/checkSuperUser.js
+++ middleware/checkSuperUser.js
@@ -1,9 +1,11 @@
 const checkSuperUser = (req, res, next) => {
     const authToken = req.token;
+    console.log(authToken);
+    console.log(process.env.SUPER_TOKEN);
     if(authToken !== process.env.SUPER_TOKEN) {
         res.status(403).send({
             success: false,
-            msg: "Invalid token"
+            msg: "Invalid super token"
         });
     } else {
         next();
